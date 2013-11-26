b7eb96eab393271d0fd9ed7ef32ae01bc14b450a
Macbook Pro 17 <Admin@MacBook-Pro-17.local>
2013-11-26 16:39:22 +0200

    Initial Commit

---
+++ Space Invaders/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Space Invaders
+//
+//  Created by Macbook Pro 17 on 11/26/13.
+//  Copyright (c) 2013 Loufis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
